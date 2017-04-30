ba7da72f0403b9ead1174890702158a08b5df7f9
Vladimir <axwitech@gmail.com>
2017-04-30 15:35:33 +0300

    added a print out method of the map, populates the neighbors of the each node

--- map.rb
+++ map.rb
@@ -10,15 +10,53 @@ $closedlist = []
 
 
 def create_map
-  $w.times do |i|
-    $h.times do |j|
-      $map[i][j] = Node.new
-      $map[i][j].x = i
-      $map[i][j].y = j
-      $map[i][j].passable = true
+  $w.times do |x|
+    $h.times do |y|
+      $map[x][y] = Node.new
+      $map[x][y].x = x
+      $map[x][y].y = y
     end
   end
 end
+#node.neighbor.push($map[row_index-1][col_index])
+def update_neighbors
+  $w.times do |x|
+    $h.times do |y|
+      if x > 0
+        $map[x][y].neighbor.push($map[x-1][y])
+      end
+
+      if x < $w-1
+        $map[x][y].neighbor.push($map[x+1][y])
+      end
+
+      if y > 0
+        $map[x][y].neighbor.push($map[x][y-1])
+      end
+
+      if y < $h-1
+        $map[x][y].neighbor.push($map[x][y+1])
+      end
+
+      if x > 0 && y > 0
+        $map[x][y].neighbor.push($map[x-1][y-1])
+      end
+
+      if x > 0 && y < $h-1
+        $map[x][y].neighbor.push($map[x-1][y+1])
+      end
+
+      if x < $w-1 && y > 0
+        $map[x][y].neighbor.push($map[x+1][y-1])
+      end
+
+      if x < $w-1 && y < $h-1
+        $map[x][y].neighbor.push($map[x+1][y+1])
+      end
+    end
+  end
+end
+
 
 def edit_map
   $map[1][3].passable = false
@@ -27,20 +65,29 @@ def edit_map
 end
 
 def print_out_map
-  $map.each do |node|
-    node.each do |realnode|
-      puts realnode.x
-      puts realnode.y
-      puts realnode.passable
+  $map.each do |row|
+    row.each do |node|
+      print "|#{node.x} #{node.y} "
+      if node.passable
+        print "."
+      else
+        print "#"
+      end
+      #puts node.passable
     end
+    puts " "
   end
 end
 
-def find_path
-  
+def find_path(star_point,end_point)
+    $start_point = map[2][1]
+    $end_point = map[2][5]
+    $openlist << $start_point
 end
 
 
 create_map()
 edit_map()
+update_neighbors()
 print_out_map
+puts $map[1][1].neighbor
--- node.rb
+++ node.rb
@@ -1,12 +1,9 @@
 class Node
+attr_accessor :passable, :score, :gscore, :kscore, :name, :x, :y, :parent, :neighbor
 
-$passable = true
-$score = 0
-$gscore = 0
-$kscore = 0
-$name = ""
-$x = 0
-$y = 0
+  def initialize
+    @passable = true
+    @neighbor = []
+  end
 
-attr_accessor :passable, :score, :gscore, :kscore, :name, :x, :y
 end
